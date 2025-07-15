7d99c1d1941810fa15271fd3ee7243708e75b07d
unknown <rohanmulik4908@gmail.com>
2025-07-15 11:57:23 -1200

    Assignment

---
+++ Assignment C/q12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char dis;
+    printf("Enter  the Character");
+    scanf("%c",&dis);
+
+    printf("\nCharacter Previos %c",dis-1);
+    printf("\nNext Character %c",dis+1);
+
+}
---
+++ Assignment C/q13.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char Display ;
+    printf("Enter Display");
+    scanf("%c",&Display);
+    printf("Askey Value %d",Display);
+
+}
